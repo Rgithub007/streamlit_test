78219f3410c2af1310aea9168342b22adb9fc044
Rgithub007 <92503564+Rgithub007@users.noreply.github.com>
2023-04-02 21:55:31 -0700

    Update streamlit_app.py
    imported pandas and added some code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+import pandas
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Salad')
 streamlit.text('Fruit Bowl')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
